d8271b968a7777cbe61d78ed91235288284bb517
Michael <mjkochka@gmail.com>
2020-12-16 09:39:37 -0800

    Added new pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import {Navbar} from 'react-bootstrap'
 import {Nav} from 'react-bootstrap'
+import {Route} from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
+import home from './Views/home.js'
+import AboutMe from './Views/AboutMe.js'
+import UnderDev from './Views/UnderDev'
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/Views/home"></Navbar.Brand>
+      <Navbar.Brand ></Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="/Views/home">Home</Nav.Link>
-        <Nav.Link href="#features">About Me</Nav.Link>
-        <Nav.Link href="#pricing">Under Development</Nav.Link>
+        <Nav.Link href="/"> Home </Nav.Link>
+        <Nav.Link href="/AboutMe">About Me</Nav.Link>
+        <Nav.Link href="/UnderDev">Under Development</Nav.Link>
       </Nav>
     </Navbar>
+    </BrowserRouter>
+
+    <BrowserRouter>
+    <div className="content">
+            <Route exact path="/" component={home}/>
+            <Route path="/AboutMe" component={AboutMe}/>
+            <Route path="/UnderDev" component={UnderDev}/>
+    </div>
+    </BrowserRouter>
     </>
   );
   
--- src/Views/AboutMe.js
+++ src/Views/AboutMe.js
@@ -2,21 +2,18 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import {Component} from 'react';
 import Button from 'react-bootstrap/Button'
-import img1 from './img1.jpg'
 
-
-class Home extends Component{
+class AboutMe extends Component{
 
 render(){
 return(
 
-<Card style={{ width: '14rem', height: '8rem' }}>
-  <Card.Img variant="top" src={img1} />
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="/img1.jpg" />
   <Card.Body>
     <Card.Title>Card Title</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+      AboutMe.
     </Card.Text>
     <Button variant="primary">View Project</Button>
   </Card.Body>
@@ -28,4 +25,4 @@ return(
 }
 }
 
-export default Home;
+export default AboutMe;
